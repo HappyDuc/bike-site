f39760a4154ec0627fe37bb6ac7ac1bf452fdbae
JakeSincl <23jsincl@glenalmondcollege.co.uk>
2023-05-14 13:22:35 +0100

    Things work!

--- api/api.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from time import time
-
-app = Flask(__name__)
-
-
-@app.route("/time")
-def get_current_time():
-    return {"time": time}
---
+++ api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import time
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.route("/time")  #
+@cross_origin()
+def get_current_time():
+    return {"time": time.time()}
